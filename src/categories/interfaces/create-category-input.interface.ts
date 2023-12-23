19a4ecdadef0965a22d731eb0b21df48aa123aee
afmireski <afmireski@outlook.com>
2023-12-23 09:19:57 -0300

    Adicionado método create a porta do CategoriesRepository

---
+++ src/categories/interfaces/create-category-input.interface.ts
@@ -0,0 +1,5 @@
+export interface CreateCategoryInput {
+  id?: string;
+  name: string;
+  description: string;
+}
--- src/categories/ports/categories-repository.port.ts
+++ src/categories/ports/categories-repository.port.ts
@@ -1,8 +1,10 @@
-import { Category } from 'kysely/kysely-types';
+import { CreateCategoryInput } from '../interfaces/create-category-input.interface';
 import { CategoryModel } from '../models/category.model';
 
 export const CATEGORIES_REPOSITORY_PORT = 'CATEGORIES_REPOSITORY_PORT';
 
 export interface CategoriesRepository {
   findById(id: string): Promise<CategoryModel>;
+
+  create(input: CreateCategoryInput): Promise<CategoryModel>;
 }
