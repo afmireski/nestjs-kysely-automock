d000265cce83c69a8d3ab73cadd67cfedd29b43a
afmireski <afmireski@outlook.com>
2024-01-03 17:49:03 -0300

    Concertada a entrada de ProductsController.update tornando todos os parâmetros opcionais

--- src/products/dtos/update-product-body.dto.ts
+++ src/products/dtos/update-product-body.dto.ts
@@ -5,21 +5,26 @@ import { UpdateProductBodyData } from '../interfaces/update-product-input.interf
 export class UpdateProductBody implements UpdateProductBodyData {
   @Validator.IsUUID('4')
   @Validator.IsNotEmpty()
+  @Validator.IsOptional()
   category_id?: string;
 
   @Validator.MaxLength(300)
   @Validator.IsNotEmpty()
+  @Validator.IsOptional()
   name?: string;
 
   @Validator.IsNotEmpty()
+  @Validator.IsOptional()
   description?: string;
 
   @Validator.IsOptional()
   @Validator.IsNotEmptyObject()
+  @Validator.IsOptional()
   details?: Record<string, unknown> | null;
 
   @Validator.IsOptional()
   @Validator.IsPositive()
   @Transformer.Transform(({ value }) => Number(value))
+  @Validator.IsOptional()
   price?: number;
 }
